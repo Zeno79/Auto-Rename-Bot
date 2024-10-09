5a078cd374e93ca1ccd71e4e769d7d3481a36cca
Zeno <144168045+Zeno79@users.noreply.github.com>
2024-10-09 18:04:07 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -3,77 +3,68 @@ from config import Config
 from .utils import send_log
 
 class Database:
-
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.madflixbotz = self._client[database_name]
-        self.col = self.madflixbotz.user
+        self.db = self._client[database_name]
+        self.users = self.db['users']  # Ensure proper collection reference
 
-    def new_user(self, id):
-        return dict(
-            _id=int(id),                                   
-            file_id=None,
-            caption=None,
-            format_template=None  # Add this line for the format template
-        )
+    def new_user(self, user_id):
+        return {
+            '_id': int(user_id),
+            'file_id': None,
+            'caption': None,
+            'format_template': None,
+            'media_type': None  # Default media type setting
+        }
 
-    async def add_user(self, b, m):
-        u = m.from_user
-        if not await self.is_user_exist(u.id):
-            user = self.new_user(u.id)
-            await self.col.insert_one(user)            
-            await send_log(b, u)
+    async def add_user(self, bot, message):
+        user_id = message.from_user.id
+        if not await self.is_user_exist(user_id):
+            user_data = self.new_user(user_id)
+            await self.users.insert_one(user_data)
+            await send_log(bot, message.from_user)
 
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({'_id': int(id)})
+    async def is_user_exist(self, user_id):
+        user = await self.users.find_one({'_id': int(user_id)})
         return bool(user)
 
+    async def set_thumbnail(self, user_id, file_id):
+        await self.users.update_one({'_id': int(user_id)}, {'$set': {'file_id': file_id}})
+
+    async def get_thumbnail(self, user_id):
+        user = await self.users.find_one({'_id': int(user_id)})
+        return user['file_id'] if user and 'file_id' in user else None
+
+    async def set_caption(self, user_id, caption):
+        await self.users.update_one({'_id': int(user_id)}, {'$set': {'caption': caption}})
+
+    async def get_caption(self, user_id):
+        user = await self.users.find_one({'_id': int(user_id)})
+        return user['caption'] if user and 'caption' in user else None
+
+    async def set_format_template(self, user_id, format_template):
+        await self.users.update_one({'_id': int(user_id)}, {'$set': {'format_template': format_template}})
+
+    async def get_format_template(self, user_id):
+        user = await self.users.find_one({'_id': int(user_id)})
+        return user['format_template'] if user and 'format_template' in user else None
+
+    async def set_media_preference(self, user_id, media_type):
+        await self.users.update_one({'_id': int(user_id)}, {'$set': {'media_type': media_type}})
+
+    async def get_media_preference(self, user_id):
+        user = await self.users.find_one({'_id': int(user_id)})
+        return user['media_type'] if user and 'media_type' in user else None
+
     async def total_users_count(self):
-        count = await self.col.count_documents({})
+        count = await self.users.count_documents({})
         return count
 
     async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
+        return self.users.find({})
 
     async def delete_user(self, user_id):
-        await self.col.delete_many({'_id': int(user_id)})
-    
-    async def set_thumbnail(self, id, file_id):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
+        await self.users.delete_many({'_id': int(user_id)})
 
-    async def get_thumbnail(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('file_id', None)
-
-    async def set_caption(self, id, caption):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
-
-    async def get_caption(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('caption', None)
-
-    async def set_format_template(self, id, format_template):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'format_template': format_template}})
-
-    async def get_format_template(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('format_template', None)
-        
-    async def set_media_preference(self, id, media_type):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'media_type': media_type}})
-        
-    async def get_media_preference(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('media_type', None)
-
-
-
+# Create a global instance of the Database class
 madflixbotz = Database(Config.DB_URL, Config.DB_NAME)
-        
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
